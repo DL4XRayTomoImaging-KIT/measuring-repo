caa70f62f2a2c3e3f52cbee5a0647f4cfc06a1fd
er5241 <er5241@anka-stroboscode.anka.kit.edu>
2022-02-02 19:33:54 +0100

    added generalized function for measuring and smaller proxy functions

--- src/measures.py
+++ src/measures.py
@@ -7,6 +7,7 @@ from itertools import combinations
 from .errors import MeasurementError
 from scipy.spatial import ConvexHull
 from einops import rearrange
+from skimage.morphology import ball, binary_dilation, binary_erosion, remove_small_holes
 import miniball
 
 def recurrent_cleaner(s):
@@ -44,7 +45,7 @@ def organ_measure(mf):
                 results.append(mf(markup == i, volume))
         except Exception as e:
             raise MeasurementError(mf.__name__, str(e)) # no meta up to now, huh?
-            
+
         return recurrent_cleaner(results)
     return wrapped_mf
 
@@ -60,42 +61,127 @@ def axial_apply(mf):
         return axial_measures
 
     return wrapped_mf
+    
+def organ_metric(markup, volume, metric, modifier=None):
+    modifiers = ['dilation', 'erosion', 'filled']
+    if modifier is not None and modifier not in modifiers:
+        raise ValueError(f'invalid modifier, must be in {modifiers}')
+
+    if modifier == 'dilation':
+        dilated_markup = binary_dilation(markup, ball(radius=10, dtype=bool))
+        markup = dilated_markup & ~markup
+    elif modifier == 'erosion':
+        markup = binary_erosion(markup, ball(radius=10, dtype=bool))
+    elif modifier == 'filled':
+        markup = remove_small_holes(markup)
+
+    if metric == 'volume':
+        measurement = markup.sum()
+    elif metric == 'surface_area':
+        measurement = find_boundaries(markup).sum()    
+    elif metric == 'mean':
+        measurement = volume[markup].mean()
+    elif metric == 'median':
+        measurement = np.median(volume[markup])    
+    elif metric == 'std':
+        measurement = volume[markup].std()
+    elif metric == 'perc_1':
+        measurement = np.percentile(volume[markup], 1)
+    elif metric == 'perc_99':
+        measurement = np.percentile(volume[markup], 99)
+
+    return measurement
 
 
 @organ_measure
 def volume(markup, volume):
     """Sums up volume of the markup"""
-    return markup.sum()
+    return organ_metric(markup, volume, 'volume')
+
+@organ_measure
+def volume_filled(markup, volume):
+    """Sums up volume of the markup after filling closed holes"""
+    return organ_metric(markup, volume, 'volume', 'filled')
 
 @organ_measure
 def surface_area(markup, volume):
     """Actually, calculates volume of pixels determined as boundary pixels by skimage"""
-    return find_boundaries(markup).sum()
+    return organ_metric(markup, volume, 'surface_area')
 
 @organ_measure
-def color_average(markup, volume):
-    """Calculates average value inside segmented organ"""
-    return volume[markup].mean()
+def color_mean(markup, volume):
+    """Calculates mean value inside segmented organ"""
+    return organ_metric(markup, volume, 'mean')
 
 @organ_measure
-def color_std(markup, volume):
-    """Calculates variance of values inside segmented organ"""
-    return volume[markup].std()
-    
+def color_mean_dilated(markup, volume):
+    """mean after dilation by 10 pixels"""
+    return organ_metric(markup, volume, 'mean', 'dilation')
+
+@organ_measure
+def color_mean_eroded(markup, volume):
+    """mean after erosion by 10 pixels"""
+    return organ_metric(markup, volume, 'mean', 'erosion')
+
 @organ_measure
 def color_median(markup, volume):
     """Calculates median of values inside segmented organ"""
-    return np.median(volume[markup])
+    return organ_metric(markup, volume, 'median')
+
+@organ_measure
+def color_median_dilated(markup, volume):
+    """median after dilation by 10 pixels"""
+    return organ_metric(markup, volume, 'median', 'dilation')
+
+@organ_measure
+def color_median_eroded(markup, volume):
+    """median after erosion by 10 pixels"""
+    return organ_metric(markup, volume, 'median', 'erosion')
+
+@organ_measure
+def color_std(markup, volume):
+    """Calculates standard deviation of values inside segmented organ"""
+    return organ_metric(markup, volume, 'std')
+
+@organ_measure
+def color_std_dilated(markup, volume):
+    """std dev. after dilation by 10 pixels"""
+    return organ_metric(markup, volume, 'std', 'dilation')
+
+@organ_measure
+def color_std_eroded(markup, volume):
+    """std dev. after erosion by 10 pixels"""
+    return organ_metric(markup, volume, 'std', 'erosion')
 
 @organ_measure
 def color_perc_99(markup, volume):
     """Calculates 99th percentile of values inside segmented organ"""
-    return np.percentile(volume[markup], 99)
+    return organ_metric(markup, volume, 'perc_99')
+
+@organ_measure
+def color_perc_99_dilated(markup, volume):
+    """99th percentile after dilation by 10 pixels"""
+    return organ_metric(markup, volume, 'perc_99', 'dilation')
+
+@organ_measure
+def color_perc_99_eroded(markup, volume):
+    """99th percentile after erosion by 10 pixels"""
+    return organ_metric(markup, volume, 'perc_99', 'erosion')
 
 @organ_measure
 def color_perc_1(markup, volume):
     """Calculates 1st percentile of values inside segmented organ"""
-    return np.percentile(volume[markup], 1)    
+    return organ_metric(markup, volume, 'perc_1')
+
+@organ_measure
+def color_perc_1_dilated(markup, volume):
+    """1st percentile after dilation by 10 pixels"""
+    return organ_metric(markup, volume, 'perc_1', 'dilation')
+
+@organ_measure
+def color_perc_1_eroded(markup, volume):
+    """1st percentile after erosion by 10 pixels"""
+    return organ_metric(markup, volume, 'perc_1', 'erosion')
 
 @organ_measure
 @axial_apply
